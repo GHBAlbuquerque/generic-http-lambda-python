a5a48df5b9e1941cb58531c4a12856ff7ca39632
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-02-25 18:15:27 -0300

    feature: implements lambda handler

--- app/lambda_handler.py
+++ app/lambda_handler.py
@@ -4,6 +4,8 @@ import requests
 
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
+from src.client.http_client import HttpClient
+
 import boto3
 
 logger = logging.getLogger()
@@ -16,9 +18,29 @@ def handle_request(event, context):
 
     logger.info(f"Handling request with correlation_id: {correlation_id}")
 
-    # Create a session with retries
-    session = requests.Session()
+    # Create a requests session with retries and timeout
+    requestSession = requests.Session()
     retries = Retry(total=retry_attempts, backoff_factor=1, status_forcelist=[500, 502, 503, 504])
-    session.mount('http://', HTTPAdapter(max_retries=retries))
+    requestSession.mount('http://', HTTPAdapter(max_retries=retries))
+    requestSession.options(timeout=timeout)
 
-    
+    # Create an HttpClient instance
+    client = HttpClient(requestSession)
+
+    try: 
+        response = client.handle_request(event.get('method'), 
+                                         event.get('url'), 
+                                         event.get('headers'), 
+                                         event.get('body')
+                                         )
+
+        logger.info(f"Response: {response}")
+        logger.info(f"Response handled sucessfully. Response status code: {response.get('status_code')}")
+
+    except requests.Exception as e:
+        logger.error("An error occurred while handling request", exc_info=True)
+        
+        return {
+            'statusCode': e.response.status_code,
+            'body': e.response.text
+        }
